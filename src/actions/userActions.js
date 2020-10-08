2bf6522b87898c3d37599c4dfe02b299ec245b0e
Luis Montero <luismontero@Luiss-MacBook-Pro.local>
2020-10-08 17:14:33 -0600

    Action creator async

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,11 @@
 
-export const GET_ALL_USERS = () => (dispatch) => {
-    dispatch({
-        type: 'GET_ALL_USERS',
-        payload: []
-    })
+export const GET_ALL_USERS = () => async (dispatch) => {
+    const userListResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (userListResponse.ok) {
+        const jsonData = await userListResponse.json();
+        dispatch({
+            type: 'GET_ALL_USERS',
+            payload: jsonData
+        })
+    }
 }
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -6,16 +6,8 @@ import * as userActions from '../actions/userActions';
 const UsersList = ({users,GET_ALL_USERS}) => {
 
     useEffect(() => {
-        // const fetchData = async () => {
-        //     const userListResponse = await fetch('https://jsonplaceholder.typicode.com/users');
-        //     if (userListResponse.ok) {
-        //         const jsonData = await userListResponse.json();
-        //         setUsers(jsonData);
-        //     }
-        // }
-        // fetchData();
         GET_ALL_USERS();
-    }, []);
+    }, [GET_ALL_USERS]);
 
     return (
         <table className="container">
